721173509d7c1b380f5dd78a72945a5ad16cf058
kozzztia <kozzztia@gmail.com>
2024-10-10 19:05:47 +0300

    proxy timer, bad but its work

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log("Project proxy initialized")
+
+const targetObject = {
+    name: 'kos',
+    age: 25,
+    gender: 'male'
+};
+
+const handler = {
+    get(target, property, receiver) {
+        if (property === 'name') {
+            document.querySelector('header').innerHTML = target[property];
+        } else if (property === 'age') {
+            document.querySelector('footer').innerHTML = target[property];
+        } else if (property === 'gender') {
+            document.querySelector('main').innerHTML = target[property];
+        }
+        console.log(property);
+        return target[property];
+    },
+    set(target, property, value, receiver) {
+        target[property] = value;
+        document.querySelector('footer').innerHTML = target[property];
+        return true;
+    }
+
+};
+
+
+const proxyObject = new Proxy(targetObject, handler);
+
+proxyObject.name;
+proxyObject.age;
+proxyObject.gender;
+
+setInterval(() => {
+
+    proxyObject.age = proxyObject.age + 1;
+}, 2000);
+
